4d83f78d3818f4a66f2adf89cf62aa9464676d59
Manuel Mollon <mmollon@itba.edu.ar>
2019-10-27 20:24:17 -0300

    ya listo el bitStreamQueue y el pit

--- projectsTP3/PIT/source/PIT.h
+++ projectsTP3/PIT/source/PIT.h
@@ -19,16 +19,20 @@
  ******************************************************************************/
 
 #define MAX_TIMERS_CANT	4
+#define NULL 0L
 
 /*******************************************************************************
  * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
  ******************************************************************************/
 
+typedef void (*pitFun_t)(void);
+
 typedef struct{
-	uint16_t timerVal[MAX_TIMERS_CANT];
+	uint32_t timerVal[MAX_TIMERS_CANT];
 	bool interruptEnable[MAX_TIMERS_CANT];
 	bool timerEnable[MAX_TIMERS_CANT];
 	bool chainMode[MAX_TIMERS_CANT];
+	pitFun_t pitCallbacks[MAX_TIMERS_CANT];
 }config_t;
 
 /*******************************************************************************
@@ -46,15 +50,12 @@ typedef struct{
 void PITinit(config_t * config);
 
 /**
- * @brief Initialize PIT driver.
- * @param config Configuration details (see config_t).
+ * @brief Modify Timer value. This change will have effect once the timer ends the current countdown.
+ * @param timerNbr Timer number to modify.
+ * @param value Period of timer in us (microseconds). Value should be between 1us to 80s. If timer is in chain mode
+ * then it is equal to number of n+1 timer cycles. In this case, values go from 0x1 to 0xFFFFFFFF.
  */
 void PITmodifyTimer(uint8_t timerNbr, uint32_t value);
-
-//TERMINAR:
-// PARSEO
-// INTERRUPCIONES
-// DEBUGEO
 
 /*******************************************************************************
  ******************************************************************************/
